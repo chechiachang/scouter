7b2ba3c40f77e111566b23a99e8704717a2e56f3
David Chang <chechiachang999@gmail.com>
2018-08-05 00:43:30 +0800

    Finish face detection python

---
+++ face_recognition/face_detection.py
@@ -0,0 +1,50 @@
+import numpy as np, glob, face_recognition, ntpath, pickle
+from shutil import copyfile
+
+
+
+def copy_face_image():
+    for i in glob.glob("data/avatars/*.jpg"):
+        image = face_recognition.load_image_file(i)
+        face_locations = face_recognition.face_locations(image)
+        
+        if face_locations:
+            print(i)
+            copyfile(i, "data/human_face/" + ntpath.basename(i))
+
+def encoding():
+    known_faces = []
+    name_index = []
+
+    for i in glob.glob("data/human_face/*.jpg"):
+        print(i)
+        image = face_recognition.load_image_file(i)
+        face_encoding = face_recognition.face_encodings(image)[0]
+        known_faces.append(face_encoding)
+        name_index.append(ntpath.basename(i))
+
+    with open('encodings', 'wb') as fp:
+        pickle.dump(known_faces, fp)
+    with open('index', 'wb') as fp:
+        pickle.dump(name_index, fp)
+
+def test_encoding():
+    with open('encodings', 'rb') as fp:
+        known_faces = pickle.load(fp)
+    with open('index', 'rb') as fp:
+        name_index = pickle.load(fp)
+
+    test_file = "data/avatars/4557.jpg"
+    image = face_recognition.load_image_file(test_file)
+    face_encoding = face_recognition.face_encodings(image)[0]
+
+    face_distances = face_recognition.face_distance(known_faces, face_encoding)
+
+    min_index = np.argmin(face_distances)
+
+    print(name_index[min_index])
+
+
+#copy_face_image
+#encoding()
+test_encoding()
